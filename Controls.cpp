d116b9ede3964a7bca026a365459340c8f470ec5
Rikin Katyal <katyalrikin@gmail.com>
2016-01-19 18:54:04 -0500

    reset speed

--- Controls.cpp
+++ Controls.cpp
@@ -14,10 +14,10 @@ Controls::Controls() {
  	this -> update();
 
  	// Initialize Talons for drive system
- 	t1 = CANTalon(DN1);
- 	t2 = CANTalon(DN2);
- 	t3 = CANTalon(DN3);
- 	t4 = CANTalon(DN4);
+ 	talon1 = CANTalon(DN1);
+ 	talon2 = CANTalon(DN2);
+ 	talon3 = CANTalon(DN3);
+ 	talon4 = CANTalon(DN4);
 
  	// Set starting variables
  	trianglesLowered = false;
@@ -34,6 +34,10 @@ Controls::~Controls() {
 }
 
 void Controls::driveBase(int x, int y) {
+
+	// Get variables from Data class
+	this -> update();
+
 	// Driving base left/right
 	if (x != 0) {
 		// Divide by 2 to moderate speed
@@ -51,6 +55,9 @@ void Controls::driveBase(int x, int y) {
 		speed3 += y / 2.0;
 		speed4 += y / 2.0;
 	}
+
+	// reset speeds to 0
+	this -> resetSpeed();
 
 }
 
@@ -93,4 +100,11 @@ void Controls::toggleTriangles() {
 
 void Controls::update() {
 	
+}
+
+void Controls::resetSpeed() {
+	speed1 = 0;
+	speed2 = 0;
+	speed3 = 0;
+	speed4 = 0;
 }
