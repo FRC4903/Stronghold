ec84aa9e02f1392f5dea3add327ec24a61e380f0
Roman Seviaryn <romansev@ymail.com>
2016-02-20 15:53:30 -0500

    Added intake sensor code

--- Resources.h
+++ Resources.h
@@ -4,7 +4,6 @@
 #include <iostream>
 //#include "Data.h"
 #include "WPILib.h"
-#include "CANTalon.h"
 
 using namespace std;
 
--- Sensors.cpp
+++ Sensors.cpp
@@ -10,58 +10,65 @@
 #include "Sensors.h"
 #include "Data.h"
 
- using namespace std;
+using namespace std;
 
- Sensors::Sensors()
- {
+Sensors::Sensors()
+{
+
  	// Sensors constructor
  	base_control = new Joystick(0);
  	secondary_control = new Joystick(1)
- 	library = Data::getInstance();
- }
 
- Sensors::~Sensors()
- {
+ 	intake_sensor = new DigitalInput(0);
+
+	library = Data::getInstance();
+}
+
+Sensors::~Sensors()
+{
  	// sensors destructor
  	delete base_control;
  	delete library;
- }
+}
 
- double Sensors::getBaseMovementInputX()
- {
+double Sensors::getBaseMovementInputX()
+{
  	// returns: x-axis of joystick for base
  	return base_control->GetRawAxis(0);
- }
+}
 
- double Sensors::getBaseMovementInputY()
- {
+double Sensors::getBaseMovementInputY()
+{
  	// returns: y-axis of joystick for base
  	return base_control->GetRawAxis(1);
- }
+}
 
- bool Sensors::getDriveControllerAButton()
- {
+bool Sensors::getDriveControllerAButton()
+{
  	return base_control->GetRawButton(1);
- }
+}
 
- bool Sensors::getSecondaryTrigger()
- {
+bool Sensors::getSecondaryTrigger()
+{
  	return secondary_control->GetRawButton(1);
- }
+}
 
- bool Sensors::getSecondarySideButton()
- {
+bool Sensors::getSecondarySideButton()
+{
  	return secondary_control->GetRawButton(2);
- }
+}
 
- bool Sensors::getSecondaryPistonUp()
- {
+bool Sensors::getSecondaryPistonUp()
+{
  	return secondary_control->GetRawButton(5);
- }
+}
 
- bool Sensors::getSecondaryPistonDown()
- {
+bool Sensors::getSecondaryPistonDown()
+{
  	return secondary_control->GetRawButton(3);
- }
+}
 
-
+bool Sensors::getIntakeSensor()
+{
+	return intake_sensor->Get()
+}
